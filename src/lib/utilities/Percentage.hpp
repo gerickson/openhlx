f93406c3d2b967ace486bcbc313bf33ddaad9113
Grant Erickson <erick205@umn.edu>
2021-08-03 11:48:56 -0700

    Initial revision.

---
+++ src/lib/utilities/Percentage.hpp
@@ -0,0 +1,45 @@
+/*
+ *    Copyright (c) 2021 Grant Erickson
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an "AS
+ *    express or implied.  See the License for the specific language
+ *    IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *    governing permissions and limitations under the License.
+ *
+ */
+
+/**
+ *    @file
+ *      This file defines...
+ *
+ */
+
+#ifndef OPENHLXPERCENTAGE_HPP
+#define OPENHLXPERCENTAGE_HPP
+
+#include <stdint.h>
+
+
+namespace HLX
+{
+
+namespace Utilities
+{
+
+typedef uint8_t Percentage;
+
+extern Percentage CalculatePercentage(const uint8_t &aNumerator, const uint8_t &aDenominator);
+
+}; // namespace Utilities
+
+}; // namespace HLX
+
+#endif // OPENHLXPERCENTAGE_HPP
