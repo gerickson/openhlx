a27a010292c95970b8ff0eace99f28b67c14a55b
Grant Erickson <erick205@umn.edu>
2021-08-10 14:03:04 -0700

    Initial revision.

---
+++ src/lib/common/FrontPanelControllerBasis.cpp
@@ -0,0 +1,55 @@
+/*
+ *    Copyright (c) 2021 Grant Erickson
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an "AS
+ *    IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *    express or implied.  See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ */
+
+/**
+ *    @file
+ *      This file implements...
+ *
+ */
+
+#include "FrontPanelControllerBasis.hpp"
+
+#include <LogUtilities/LogUtilities.hpp>
+
+#include <OpenHLX/Utilities/Assert.hpp>
+
+
+using namespace Nuovations;
+
+
+namespace HLX
+{
+
+namespace Common
+{
+
+Status
+FrontPanelControllerBasis :: Init(void)
+{
+    Status lRetval = kStatus_Success;
+
+    lRetval = mFrontPanelModel.Init();
+    nlREQUIRE_SUCCESS(lRetval, done);
+
+done:
+    return (lRetval);
+}
+
+}; // namespace Common
+
+}; // namespace HLX
---
+++ src/lib/common/NetworkControllerBasis.cpp
@@ -0,0 +1,55 @@
+/*
+ *    Copyright (c) 2021 Grant Erickson
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an "AS
+ *    IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *    express or implied.  See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ */
+
+/**
+ *    @file
+ *      This file implements...
+ *
+ */
+
+#include "NetworkControllerBasis.hpp"
+
+#include <LogUtilities/LogUtilities.hpp>
+
+#include <OpenHLX/Utilities/Assert.hpp>
+
+
+using namespace Nuovations;
+
+
+namespace HLX
+{
+
+namespace Common
+{
+
+Status
+NetworkControllerBasis :: Init(void)
+{
+    Status lRetval = kStatus_Success;
+
+    lRetval = mNetworkModel.Init();
+    nlREQUIRE_SUCCESS(lRetval, done);
+
+done:
+    return (lRetval);
+}
+
+}; // namespace Common
+
+}; // namespace HLX
