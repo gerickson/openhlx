1a0bc91f47dbacff92f35a9a980ece679e0b94cc
Grant Erickson <erick205@umn.edu>
2021-08-13 13:11:31 -0700

    Initial revision.

---
+++ src/lib/common/ConfigurationControllerBasis.cpp
@@ -0,0 +1,51 @@
+/*
+ *    Copyright (c) 2021 Grant Erickson
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an "AS
+ *    IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *    express or implied.  See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ */
+
+/**
+ *    @file
+ *      This file implements a base object for realizing a HLX
+ *      configuration controller, either in a client or server
+ *      context.
+ *
+ */
+
+#include "ConfigurationControllerBasis.hpp"
+
+#include <LogUtilities/LogUtilities.hpp>
+
+#include <OpenHLX/Utilities/Assert.hpp>
+
+
+using namespace Nuovations;
+
+
+namespace HLX
+{
+
+namespace Common
+{
+
+Status
+ConfigurationControllerBasis :: Init(void)
+{
+    return (kStatus_Success);
+}
+
+}; // namespace Common
+
+}; // namespace HLX
