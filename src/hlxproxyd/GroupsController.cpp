60022743fa9a350decc305da76515d2d93d4fe11
Grant Erickson <erick205@umn.edu>
2021-08-15 15:17:32 -0700

    Initial stub implementation.

---
+++ src/hlxproxyd/GroupsController.cpp
@@ -0,0 +1,251 @@
+/*
+ *    Copyright (c) 2021 Grant Erickson
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an "AS
+ *    IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *    express or implied.  See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ */
+
+/**
+ *    @file
+ *      This file implements an object for....
+ *
+ */
+
+#include "GroupsController.hpp"
+
+#include <memory>
+
+#include <errno.h>
+
+#include <LogUtilities/LogUtilities.hpp>
+
+#include <OpenHLX/Utilities/Assert.hpp>
+#include <OpenHLX/Utilities/ElementsOf.hpp>
+
+#include "ProxyCommand.hpp"
+
+
+using namespace HLX::Common;
+using namespace HLX::Model;
+using namespace HLX::Server;
+using namespace HLX::Utilities;
+using namespace Nuovations;
+
+
+namespace HLX
+{
+
+namespace Proxy
+{
+
+/**
+ *  @brief
+ *    This is the class default constructor.
+ *
+ */
+GroupsController :: GroupsController(void) :
+    Proxy::ControllerBasis(),
+    Common::GroupsControllerBasis(),
+    Client::GroupsControllerBasis(),
+    Server::GroupsControllerBasis()
+{
+    return;
+}
+
+/**
+ *  @brief
+ *    This is the class destructor.
+ *
+ */
+GroupsController :: ~GroupsController(void)
+{
+    return;
+}
+
+/**
+ *  @brief
+ *    Register or unregister notification handlers.
+ *
+ *  This registers or unregisters the solicited and unsolicited client
+ *  command response notification handlers that this controller is
+ *  interested in and will handle on behalf of the client.
+ *
+ *  @param[in]  aRegister  Indicates whether to register (true) or
+ *                         unregister (false) the handlers.
+ *
+ *  @retval  kStatus_Success              If successful.
+ *  @retval  -EINVAL                      If either of the handler iterators
+ *                                        was null.
+ *  @retval  -EEXIST                      If a registration already exists.
+ *  @retval  -ENOENT                      If there was no such handler
+ *                                        registration to unregister.
+ *  @retval  kError_NotInitialized        The base class was not properly
+ *                                        initialized.
+ *  @retval  kError_InitializationFailed  If initialization otherwise failed.
+ *
+ */
+Status
+GroupsController :: DoNotificationHandlers(const bool &aRegister)
+{
+    Status                   lRetval = kStatus_Success;
+
+    return (lRetval);
+}
+
+Status
+GroupsController :: DoRequestHandlers(const bool &aRegister)
+{
+    Status                   lRetval = kStatus_Success;
+
+    return (lRetval);
+}
+
+/**
+ *  @brief
+ *    This is the class initializer.
+ *
+ *  This initializes the class with the specified command manager and
+ *  timeout.
+ *
+ *  @param[in]  aCommandManager  A reference to the command manager
+ *                               instance to initialize the controller
+ *                               with.
+ *  @param[in]  aTimeout         The timeout to initialize the controller
+ *                               with that will serve as the timeout for
+ *                               future operations with the peer server.
+ *
+ *  @retval  kStatus_Success              If successful.
+ *  @retval  -EINVAL                      If an internal parameter was
+ *                                        invalid.
+ *  @retval  -ENOMEM                      If memory could not be allocated.
+ *  @retval  kError_NotInitialized        The base class was not properly
+ *                                        initialized.
+ *  @retval  kError_InitializationFailed  If initialization otherwise failed.
+ *
+ */
+Status
+GroupsController :: Init(Client::CommandManager &aClientCommandManager, Server::CommandManager &aServerCommandManager, const Timeout &aTimeout)
+{
+    DeclareScopedFunctionTracer(lTracer);
+    constexpr bool  kRegister = true;
+    Status          lRetval = kStatus_Success;
+
+
+    lRetval = Common::GroupsControllerBasis::Init();
+    nlREQUIRE_SUCCESS(lRetval, done);
+
+    lRetval = Client::GroupsControllerBasis::Init();
+    nlREQUIRE_SUCCESS(lRetval, done);
+
+    lRetval = Server::GroupsControllerBasis::Init();
+    nlREQUIRE_SUCCESS(lRetval, done);
+
+    lRetval = Proxy::ControllerBasis::Init(aClientCommandManager, aServerCommandManager, aTimeout);
+    nlREQUIRE_SUCCESS(lRetval, done);
+
+    // These MUST come AFTER the base class initialization due to a
+    // dependency on the command manager instance.
+
+    lRetval = DoNotificationHandlers(kRegister);
+    nlREQUIRE_SUCCESS(lRetval, done);
+
+    lRetval = DoRequestHandlers(kRegister);
+    nlREQUIRE_SUCCESS(lRetval, done);
+
+done:
+    return (lRetval);
+}
+
+/**
+ *  @brief
+ *    Refresh or obtain an up-to-date view of the server peer state.
+ *
+ *  This attempts to refresh or obtain an up-to-date view of the
+ *  server peer state with the specified timeout.
+ *
+ *  Presently, this controller does so by executing a "query
+ *  group [QGn]" command with the peer server.
+ *
+ *  @param[in]  aTimeout  The timeout to use for the refresh operation
+ *                        with the peer server.
+ *
+ *  @retval  kStatus_Success              If successful.
+ *  @retval  -ENOMEM                      If memory could not be allocated
+ *                                        for the command exchange or
+ *                                        exchange state.
+ *  @retval  kError_InitializationFailed  If initialization otherwise failed.
+ *
+ */
+Status
+GroupsController :: Refresh(const Timeout &aTimeout)
+{
+    DeclareScopedFunctionTracer(lTracer);
+    Status lRetval = kStatus_Success;
+
+
+    (void)aTimeout;
+
+    mGroupsDidRefreshCount = 0;
+
+    // Notify the base controller that we have begun a refresh
+    // operation.
+
+    SetRefreshRequested(true);
+
+done:
+    return (lRetval);
+}
+
+// MARK: Configuration Management Methods
+
+Status
+GroupsController :: QueryCurrentConfiguration(Server::ConnectionBasis &aConnection, ConnectionBuffer::MutableCountedPointer &aBuffer)
+{
+    DeclareScopedFunctionTracer(lTracer);
+    Status                                lRetval = kStatus_Success;
+
+
+    (void)aConnection;
+
+    // For each group, query the configuration.
+
+done:
+    return (lRetval);
+}
+
+// MARK: Server-facing Client Observer Methods
+
+// MARK: Server-facing Client Command Completion Handlers
+
+// MARK: Server-facing Client Command Completion Handler Trampolines
+
+// MARK: Server-facing Client Unsolicited Notification Handlers
+
+// MARK: Server-facing Client Unsolicited Notification Handler Trampolines
+
+// MARK: Client-facing Server Command Request Completion Handlers
+
+// MARK: Client-facing Server Command Request Handler Trampolines
+
+// MARK: Proxy Handlers
+
+// MARK: Proxy Handler Trampolines
+
+// MARK: Server-facing Client Implementation
+
+// MARK: Client-facing Server Implementation
+
+}; // namespace Proxy
+
+}; // namespace HLX
