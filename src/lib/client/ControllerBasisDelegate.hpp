7f8fd3afae1902b62457ce8ce41eb7ad1e0f1449
Grant Erickson <erick205@umn.edu>
2021-08-21 15:03:47 -0700

    Split the controller basis delegate into three: error, refresh, and state change delegates and made this file an umbrella header.

--- src/lib/client/ControllerBasisDelegate.hpp
+++ src/lib/client/ControllerBasisDelegate.hpp
@@ -18,97 +18,16 @@
 
 /**
  *    @file
- *      This file defines a delegate interface for the client
- *      controller basis object.
+ *      This file is an umbrella header for delegate interfaces for
+ *      the client controller basis object.
  *
  */
 
 #ifndef OPENHLXCLIENTCONTROLLERBASISDELEGATE_HPP
 #define OPENHLXCLIENTCONTROLLERBASISDELEGATE_HPP
 
-#include <OpenHLX/Client/StateChangeNotificationBasis.hpp>
-
-
-namespace HLX
-{
-
-namespace Client
-{
-
-class ControllerBasis;
-
-/**
- *  @brief
- *    A delegate interface for the client controller basis object.
- *
- *  @ingroup client
- *
- */
-class ControllerBasisDelegate
-{
-public:
-    ControllerBasisDelegate(void) = default;
-    ~ControllerBasisDelegate(void) = default;
-
-    /**
-     *  @brief
-     *    Delegation from a controller that the controller is
-     *    refreshing.
-     *
-     *  @param[in]  aController       A reference to the controller
-     *                                that issued the delegation.
-     *  @param[in]  aPercentComplete  A reference to the percentage
-     *                                (0-100) of the refresh operation
-     *                                that has completed.
-     *
-     */
-    virtual void ControllerIsRefreshing(ControllerBasis &aController, const uint8_t &aPercentComplete) = 0;
-
-    /**
-     *  @brief
-     *    Delegation from a controller that the controller is
-     *    done refreshing.
-     *
-     *  @param[in]  aController       A reference to the controller
-     *                                that issued the delegation.
-     *
-     */
-    virtual void ControllerDidRefresh(ControllerBasis &aController) = 0;
-
-    /**
-     *  @brief
-     *    Delegation from a controller that the controller state
-     *    has changed in response to a change from the peer server
-     *    controller.
-     *
-     *  @param[in]  aController               A reference to the
-     *                                        controller that issued
-     *                                        the delegation.
-     *  @param[in]  aStateChangeNotification  An immutable reference
-     *                                        to a notification
-     *                                        describing the state
-     *                                        change.
-     *
-     */
-    virtual void ControllerStateDidChange(ControllerBasis &aController, const StateChange::NotificationBasis &aStateChangeNotification) = 0;
-
-    /**
-     *  @brief
-     *    Delegation from a controller that the controller experienced
-     *    an error.
-     *
-     *  @param[in]  aController  A reference to the controller that
-     *                           issued the delegation.
-     *  @param[in]  aError       An immutable reference to the error
-     *                           associated with the event.
-     *
-     */
-    virtual void ControllerError(ControllerBasis &aController, const Common::Error &aError) = 0;
-
-};
-
-}; // namespace Client
-
-}; // namespace HLX
+#include <OpenHLX/Client/ControllerBasisErrorDelegate.hpp>
+#include <OpenHLX/Client/ControllerBasisRefreshDelegate.hpp>
+#include <OpenHLX/Client/ControllerBasisStateChangeDelegate.hpp>
 
 #endif // OPENHLXCLIENTCONTROLLERBASISDELEGATE_HPP
