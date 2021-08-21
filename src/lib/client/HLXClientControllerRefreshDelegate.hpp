cfd0669770f3aceb687a23bacca998832971add7
Grant Erickson <erick205@umn.edu>
2021-08-21 15:04:20 -0700

    Initial revision from HLXClientControllerDelegate.hpp.

---
+++ src/lib/client/HLXClientControllerRefreshDelegate.hpp
@@ -0,0 +1,117 @@
+/*
+ *    Copyright (c) 2018-2021 Grant Erickson
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an "AS
+ *    IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *    express or implied.  See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ */
+
+/**
+ *    @file
+ *      This file defines a refresh delegate interface for the top-
+ *      level HLX client controller object.
+ *
+ */
+
+#ifndef OPENHLXCLIENTCONTROLLERREFRESHDELEGATE_HPP
+#define OPENHLXCLIENTCONTROLLERREFRESHDELEGATE_HPP
+
+
+#include <OpenHLX/Common/Errors.hpp>
+
+
+namespace HLX
+{
+
+namespace Client
+{
+
+class Controller;
+
+/**
+ *  @brief
+ *    A refresh delegate interface for the top-level HLX client
+ *    controller object.
+ *
+ *  This delegate interface allows interested delegates to receive
+ *  notifications regarding the state of the client as it
+ *  moves through its lifetime, including:
+ *
+ *    - The state of its data model.
+ *
+ *  @ingroup client
+ *
+ */
+class ControllerRefreshDelegate
+{
+public:
+    ControllerRefreshDelegate(void) = default;
+    virtual ~ControllerRefreshDelegate(void) = default;
+
+    // Refresh Delegation Methods
+
+    /**
+     *  @brief
+     *    Delegation from the client controller that a state refresh
+     *    with the peer server is about to begin.
+     *
+     *  @param[in]  aController  A reference to the client controller that
+     *                           issued the delegation.
+     *
+     */
+    virtual void ControllerWillRefresh(Controller &aController) = 0;
+
+    /**
+     *  @brief
+     *    Delegation from the client controller that a state refresh
+     *    with the peer server is in progress.
+     *
+     *  @param[in]  aController       A reference to the client controller
+     *                                that issued the delegation.
+     *  @param[in]  aPercentComplete  A reference to the percentage
+     *                                (0-100) of the refresh operation
+     *                                that has completed.
+     *
+     */
+    virtual void ControllerIsRefreshing(Controller &aController, const uint8_t &aPercentComplete) = 0;
+
+    /**
+     *  @brief
+     *    Delegation from the client controller that a state refresh
+     *    with the peer server did complete successfully.
+     *
+     *  @param[in]  aController  A reference to the client controller that
+     *                           issued the delegation.
+     *
+     */
+    virtual void ControllerDidRefresh(Controller &aController) = 0;
+
+    /**
+     *  @brief
+     *    Delegation from the client controller that a state refresh
+     *    with the peer server did not complete successfully.
+     *
+     *  @param[in]  aController  A reference to the client controller that
+     *                           issued the delegation.
+     *  @param[in]  aError       An immutable reference to the error
+     *                           associated with the failure to refresh.
+     *
+     */
+    virtual void ControllerDidNotRefresh(Controller &aController, const Common::Error &aError) = 0;
+};
+
+}; // namespace Client
+
+}; // namespace HLX
+
+#endif // OPENHLXCLIENTCONTROLLERREFRESHDELEGATE_HPP
