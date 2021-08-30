554437a0be45602570d122f944feec9ad4bba48d
Grant Erickson <erick205@umn.edu>
2021-08-29 17:07:04 -0700

    Promote 'DoNotificationHandlers' from 'private' to 'protected' visibility and and reserve calling it for derived classes.

--- src/lib/client/FavoritesControllerBasis.hpp
+++ src/lib/client/FavoritesControllerBasis.hpp
@@ -79,11 +79,17 @@ protected:
 
     static void NameNotificationReceivedHandler(const uint8_t *aBuffer, const size_t &aSize, const Common::RegularExpression::Matches &aMatches, void *aContext);
 
+protected:
+    // Implementation
+
+    Common::Status DoNotificationHandlers(const bool &aRegister);
+
 private:
     // Command Completion Handlers
 
     void QueryCompleteHandler(Command::ExchangeBasis::MutableCountedPointer &aExchange, const Common::RegularExpression::Matches &aMatches);
     void SetNameCompleteHandler(Command::ExchangeBasis::MutableCountedPointer &aExchange, const Common::RegularExpression::Matches &aMatches);
+
     void CommandErrorHandler(Command::ExchangeBasis::MutableCountedPointer &aExchange, const Common::Error &aError);
 
     // Notification Handlers
@@ -93,7 +99,6 @@ private:
 private:
     // Implementation
 
-    Common::Status DoNotificationHandlers(const bool &aRegister);
     Common::Status ResponseInit(void);
 
 private:
