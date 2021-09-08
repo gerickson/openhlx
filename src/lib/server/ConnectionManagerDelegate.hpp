74bf0c3b5905a1474f017d6891b983af9c89ba97
Grant Erickson <erick205@umn.edu>
2021-09-08 12:14:52 -0700

    Doxygenated public types and interfaces.

--- src/lib/server/ConnectionManagerDelegate.hpp
+++ src/lib/server/ConnectionManagerDelegate.hpp
@@ -52,23 +52,140 @@ class ConnectionManagerDelegate :
     public Common::ConnectionManagerDelegateBasis
 {
 public:
+    // Con/destructor
+
     ConnectionManagerDelegate(void) = default;
     ~ConnectionManagerDelegate(void) = default;
 
-    // Listen
+    // Listen Methods
 
+    /**
+     *  @brief
+     *    Delegation from the connection manager that it will listen
+     *    for connections from peer clients at the specified URL.
+     *
+     *  @param[in]  aConnectionManager  A reference to the connection
+     *                                  manager that issued the
+     *                                  delegation.
+     *  @param[in]  aURLRef             The URL at which the server is
+     *                                  to listen at.
+     *
+     */
     virtual void ConnectionManagerWillListen(ConnectionManager &aConnectionManager, CFURLRef aURLRef) = 0;
+
+    /**
+     *  @brief
+     *    Delegation from the connection manager that it is in the
+     *    processing of listening for connections from peer clients at
+     *    the specified URL.
+     *
+     *  @param[in]  aConnectionManager  A reference to the connection
+     *                                  manager that issued the
+     *                                  delegation.
+     *  @param[in]  aURLRef             The URL at which the server is
+     *                                  listening at.
+     *
+     */
     virtual void ConnectionManagerIsListening(ConnectionManager &aConnectionManager, CFURLRef aURLRef) = 0;
+
+    /**
+     *  @brief
+     *    Delegation from the connection manager that it is listening
+     *    for connections from peer clients at the specified URL.
+     *
+     *  @param[in]  aConnectionManager  A reference to the connection
+     *                                  manager that issued the
+     *                                  delegation.
+     *  @param[in]  aURLRef             The URL at which the server is
+     *                                  listening at.
+     *
+     */
     virtual void ConnectionManagerDidListen(ConnectionManager &aConnectionManager, CFURLRef aURLRef) = 0;
+
+    /**
+     *  @brief
+     *    Delegation from the connection manager that it did not
+     *    listen for connections from peer clients at the specified
+     *    URL.
+     *
+     *  @param[in]  aConnectionManager  A reference to the connection
+     *                                  manager that issued the
+     *                                  delegation.
+     *  @param[in]  aURLRef             The URL at which the server
+     *                                  was to listen at.
+     *  @param[in]  aError              An immutable reference to the
+     *                                  error associated with the
+     *                                  failed server listen.
+     *
+     */
     virtual void ConnectionManagerDidNotListen(ConnectionManager &aConnectionManager, CFURLRef aURLRef, const Common::Error &aError) = 0;
 
-    // Accept
+    // Accept Methods
 
+    /**
+     *  @brief
+     *    Delegation from the connection manager that it will accept
+     *    a connection from the peer client at the specified URL.
+     *
+     *  @param[in]  aConnectionManager  A reference to the connection
+     *                                  manager that issued the
+     *                                  delegation.
+     *  @param[in]  aURLRef             The URL for the client from
+     *                                  which the server will accept a
+     *                                  connection.
+     *
+     */
     virtual void ConnectionManagerWillAccept(ConnectionManager &aConnectionManager, CFURLRef aURLRef) = 0;
-    virtual void ConnectionManagerIsAccepting(ConnectionManager &aConnectionManager, CFURLRef aURLRef) = 0;
-    virtual void ConnectionManagerDidAccept(ConnectionManager &aConnectionManager, CFURLRef aURLRef) = 0;
-    virtual void ConnectionManagerDidNotAccept(ConnectionManager &aConnectionManager, CFURLRef aURLRef, const Common::Error &aError) = 0;
 
+    /**
+     *  @brief
+     *    Delegation from the connection manager that it is in the
+     *    process of accepting a connection from the peer client at
+     *    the specified URL.
+     *
+     *  @param[in]  aConnectionManager  A reference to the connection
+     *                                  manager that issued the
+     *                                  delegation.
+     *  @param[in]  aURLRef             The URL for the client from
+     *                                  which the server is accepting
+     *                                  a connection.
+     *
+     */
+    virtual void ConnectionManagerIsAccepting(ConnectionManager &aConnectionManager, CFURLRef aURLRef) = 0;
+
+    /**
+     *  @brief
+     *    Delegation from the connection manager that it did accept
+     *    a connection from the peer client at the specified URL.
+     *
+     *  @param[in]  aConnectionManager  A reference to the connection
+     *                                  manager that issued the
+     *                                  delegation.
+     *  @param[in]  aURLRef             The URL for the client from
+     *                                  which the server did accept a
+     *                                  connection.
+     *
+     */
+    virtual void ConnectionManagerDidAccept(ConnectionManager &aConnectionManager, CFURLRef aURLRef) = 0;
+
+    /**
+     *  @brief
+     *    Delegation from the connection manager that it did not
+     *    accept a connection from the peer client at the specified
+     *    URL.
+     *
+     *  @param[in]  aConnectionManager  A reference to the connection
+     *                                  manager that issued the
+     *                                  delegation.
+     *  @param[in]  aURLRef             The URL for the client from
+     *                                  which the server did not
+     *                                  accept a connection.
+     *  @param[in]  aError              An immutable reference to the
+     *                                  error associated with the
+     *                                  failed server accept.
+     *
+     */
+    virtual void ConnectionManagerDidNotAccept(ConnectionManager &aConnectionManager, CFURLRef aURLRef, const Common::Error &aError) = 0;
 };
 
 }; // namespace Server
