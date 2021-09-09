9068c32d1945c9139f4dffe1f620a243edddf0f3
Grant Erickson <erick205@umn.edu>
2021-09-08 17:44:42 -0700

    Doxygenated public interfaces.

--- src/lib/server/CommandVolumeResponseBases.cpp
+++ src/lib/server/CommandVolumeResponseBases.cpp
@@ -46,22 +46,108 @@ namespace Server
 namespace Command
 {
 
-Status VolumeResponseBasis :: Init(const char *aObject, const IdentifierModel::IdentifierType &aIdentifier, const LevelType &aVolume)
+/**
+ *  @brief
+ *    This is the class initializer for a volume level response.
+ *
+ *  This initializes a volume level property response against a
+ *  specific object identifier.
+ *
+ *  @param[in]  aObject      A pointer to a null-terminated C string
+ *                           representing the object for which the
+ *                           volume level response is to be
+ *                           formed. For example, "O" for a zone
+ *                           object.
+ *  @param[in]  aIdentifier  A reference to the specific object
+ *                           identifier for which the volume level
+ *                           response is to be formed.
+ *  @param[in]  aLevel       An immutable reference to the
+ *                           volume level in the response.
+ *
+ *  @retval  kStatus_Success  If successful.
+ *
+ */
+Status
+VolumeResponseBasis :: Init(const char *aObject, const IdentifierModel::IdentifierType &aIdentifier, const LevelType &aVolume)
 {
     return (VolumeBufferBasis::Init(*this, aObject, aIdentifier, aVolume));
 }
 
-Status VolumeAllResponseBasis :: Init(const char *aObject, const LevelType &aVolume)
+/**
+ *  @brief
+ *    This is the class initializer for an all-object volume level
+ *    response.
+ *
+ *  This initializes an all-object volume level property response.
+ *
+ *  @param[in]  aObject      A pointer to a null-terminated C string
+ *                           representing the objecst for which the
+ *                           volume level response is to be
+ *                           formed. For example, "O" for zone
+ *                           objects.
+ *  @param[in]  aLevel       An immutable reference to the
+ *                           volume level in the response.
+ *
+ *  @retval  kStatus_Success  If successful.
+ *
+ */
+Status
+VolumeAllResponseBasis :: Init(const char *aObject, const LevelType &aVolume)
 {
     return (VolumeAllBufferBasis::Init(*this, aObject, aVolume));
 }
 
-Status VolumeFixedResponseBasis :: Init(const char *aObject, const IdentifierType &aIdentifier, const FixedType &aVolumeFixed)
+/**
+ *  @brief
+ *    This is the class initializer for a volume mute state response.
+ *
+ *  This initializes a volume fixed/locked state response against a
+ *  specific object identifier
+ *
+ *  @param[in]  aObject      A pointer to a null-terminated C string
+ *                           representing the object for which the
+ *                           volume fixed/locked state response is to
+ *                           be formed. For example, "O" for a zone
+ *                           object.
+ *  @param[in]  aIdentifier  A reference to the specific object
+ *                           identifier for which the volume
+ *                           fixed/locked state response is to be
+ *                           formed.
+ *  @param[in]  aMute        An immutable reference to the volume
+ *                           fixed/locked state in the response.
+ *
+ *  @retval  kStatus_Success  If successful.
+ *
+ */
+Status
+VolumeFixedResponseBasis :: Init(const char *aObject, const IdentifierType &aIdentifier, const FixedType &aVolumeFixed)
 {
     return (VolumeFixedBufferBasis::Init(*this, aObject, aIdentifier, aVolumeFixed));
 }
 
-Status VolumeMuteResponseBasis :: Init(const char *aObject, const IdentifierType &aIdentifier, const MuteType &aMute)
+/**
+ *  @brief
+ *    This is the class initializer for a volume mute state response.
+ *
+ *  This initializes a volume mute state response against a specific
+ *  object identifier
+ *
+ *  @param[in]  aObject      A pointer to a null-terminated C string
+ *                           representing the object for which the
+ *                           volume mute state response is to be
+ *                           formed. For example, "O" for a zone
+ *                           object.
+ *  @param[in]  aIdentifier  A reference to the specific object
+ *                           identifier for which the volume mute state
+ *                           response is to be formed.
+ *  @param[in]  aMute        An immutable reference to the volume
+ *                           mute state in the response.
+ *
+ *  @retval  kStatus_Success  If successful.
+ *
+ */
+Status
+VolumeMuteResponseBasis :: Init(const char *aObject, const IdentifierType &aIdentifier, const MuteType &aMute)
 {
     return (VolumeMuteBufferBasis::Init(*this, aObject, aIdentifier, aMute));
 }
