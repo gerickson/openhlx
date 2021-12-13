42c8067b93160281185d263b3705cc7ee8c3877a
Grant Erickson <erick205@umn.edu>
2021-12-13 13:32:10 -0800

    White space changes.

--- src/lib/client/FrontPanelControllerBasis.cpp
+++ src/lib/client/FrontPanelControllerBasis.cpp
@@ -713,8 +713,8 @@ FrontPanelControllerBasis :: BrightnessNotificationReceivedHandler(const uint8_t
 
 /**
  *  @brief
- *    Front panel disabled/locked changed client unsolicited notification handler
- *    trampoline.
+ *    Front panel disabled/locked changed client unsolicited
+ *    notification handler trampoline.
  *
  *  This invokes the handler for an unsolicited, asynchronous client
  *  notification for the front panel disabled/locked changed notification.
