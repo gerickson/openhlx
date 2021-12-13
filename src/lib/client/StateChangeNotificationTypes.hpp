3ee25edb3f000c0f3c41313fbe99b8f23192d29f
Grant Erickson <erick205@umn.edu>
2021-12-13 12:54:30 -0800

    Added 'kStateChangeType_NetworkDHCPv4Enabled' and 'kStateChangeType_NetworkSDDPEnabled' types.

--- src/lib/client/StateChangeNotificationTypes.hpp
+++ src/lib/client/StateChangeNotificationTypes.hpp
@@ -45,66 +45,69 @@ namespace StateChange
  */
 enum Type
 {
-    kStateChangeType_Min                           =   0,
+    kStateChangeType_Min                              =   0,
 
-    kStateChangeType_Unknown                       = kStateChangeType_Min,
+    kStateChangeType_Unknown                          = kStateChangeType_Min,
 
     // Configuration
 
-    kStateChangeType_ConfigurationLoadedFromBackup = 100,
-    kStateChangeType_ConfigurationResetToDefaults  = 101,
-    kStateChangeType_ConfigurationSavingToBackup   = 102,
-    kStateChangeType_ConfigurationSavedToBackup    = 103,
+    kStateChangeType_ConfigurationLoadedFromBackup    = 100,
+    kStateChangeType_ConfigurationResetToDefaults     = 101,
+    kStateChangeType_ConfigurationSavingToBackup      = 102,
+    kStateChangeType_ConfigurationSavedToBackup       = 103,
 
     // Equalizer Presets
 
-    kStateChangeType_EqualizerPresetBand           = 201,
-    kStateChangeType_EqualizerPresetName           = 202,
+    kStateChangeType_EqualizerPresetBand              = 201,
+    kStateChangeType_EqualizerPresetName              = 202,
 
     // Favorites
 
-    kStateChangeType_FavoriteName                  = 301,
+    kStateChangeType_FavoriteName                     = 301,
 
     // Front Panel
 
-    kStateChangeType_FrontPanelBrightness          = 401,
-    kStateChangeType_FrontPanelLocked              = 402,
+    kStateChangeType_FrontPanelBrightness             = 401,
+    kStateChangeType_FrontPanelLocked                 = 402,
 
     // Groups
 
-    kStateChangeType_GroupMute                     = 500,
-    kStateChangeType_GroupName                     = 501,
-    kStateChangeType_GroupSource                   = 502,
-    kStateChangeType_GroupVolume                   = 503,
-    kStateChangeType_GroupIncreaseVolume           = 504,
-    kStateChangeType_GroupDecreaseVolume           = 505,
-    kStateChangeType_GroupZoneAdded                = 506,
-    kStateChangeType_GroupZoneRemoved              = 507,
+    kStateChangeType_GroupMute                        = 500,
+    kStateChangeType_GroupName                        = 501,
+    kStateChangeType_GroupSource                      = 502,
+    kStateChangeType_GroupVolume                      = 503,
+    kStateChangeType_GroupIncreaseVolume              = 504,
+    kStateChangeType_GroupDecreaseVolume              = 505,
+    kStateChangeType_GroupZoneAdded                   = 506,
+    kStateChangeType_GroupZoneRemoved                 = 507,
 
     // Infrared
 
-    kStateChangeType_InfraredDisabled              = 601,
+    kStateChangeType_InfraredDisabled                 = 601,
 
     // Network
 
+    kStateChangeType_NetworkDHCPv4Enabled             = 700,
+    kStateChangeType_NetworkSDDPEnabled               = 701,
+
     // Sources
 
-    kStateChangeType_SourceName                    = 800,
+    kStateChangeType_SourceName                       = 800,
 
     // Zones
 
-    kStateChangeType_ZoneBalance                   = 900,
-    kStateChangeType_ZoneEqualizerBand             = 901,
-    kStateChangeType_ZoneEqualizerPreset           = 902,
-    kStateChangeType_ZoneHighpassCrossover         = 903,
-    kStateChangeType_ZoneLowpassCrossover          = 904,
-    kStateChangeType_ZoneMute                      = 905,
-    kStateChangeType_ZoneName                      = 906,
-    kStateChangeType_ZoneSoundMode                 = 907,
-    kStateChangeType_ZoneSource                    = 908,
-    kStateChangeType_ZoneTone                      = 909,
-    kStateChangeType_ZoneVolume                    = 910,
-    kStateChangeType_ZoneVolumeLocked              = 911,
+    kStateChangeType_ZoneBalance                      = 900,
+    kStateChangeType_ZoneEqualizerBand                = 901,
+    kStateChangeType_ZoneEqualizerPreset              = 902,
+    kStateChangeType_ZoneHighpassCrossover            = 903,
+    kStateChangeType_ZoneLowpassCrossover             = 904,
+    kStateChangeType_ZoneMute                         = 905,
+    kStateChangeType_ZoneName                         = 906,
+    kStateChangeType_ZoneSoundMode                    = 907,
+    kStateChangeType_ZoneSource                       = 908,
+    kStateChangeType_ZoneTone                         = 909,
+    kStateChangeType_ZoneVolume                       = 910,
+    kStateChangeType_ZoneVolumeLocked                 = 911,
 
     kStateChangeType_Max
 };
